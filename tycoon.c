81c158c354fd21d0a50210f4d7c720947a03227b
eoratyoter <a01043666757@gmail.com>
2026-03-24 11:20:35 +0900

    c

--- tycoon.c
+++ tycoon.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #define ROW 2
-#define COL 2
+#define COL 3
 #define MAX_CUPSIZE 5
 int ice_num = 0;
 
@@ -15,7 +15,9 @@ typedef struct icecream{
     int stock;
 }Icecream;
 
-Icecream* icecream_tb[ROW][COL];
+Icecream*** icecream_tb;
+int reverse;
+
 typedef struct customer{
     int cstmrnum;
     int cup_size;
@@ -34,9 +36,21 @@ Icecream* create_icecream(){
     memset(ice, 0, sizeof(Icecream));
     return ice;
 }
+Icecream*** create_icecream_table(int row, int col){
+    int i;
+    Icecream*** table;
+    table = (Icecream***) malloc (sizeof(Icecream**) * row);
+    for (i=0; i<row; i++){
+        table[i] = (Icecream**) malloc (sizeof(Icecream*) * col);
+    }
+    return table;
+}
 
 void table_init(){
     int i, j;
+    int ice_num = 0;
+    icecream_tb = create_icecream_table(ROW, COL);
+    reverse = 0;
     char name[15];
     srand(time(NULL));
     for(i = 0; i < ROW; i++){
@@ -52,12 +66,36 @@ void table_init(){
     }
 }
 
+void transposed_icecream(){
+    int row = ROW;
+    int col = COL;
+    int i, j;
+    if (reverse == 1){
+        row = COL;
+        col = ROW;
+    }
+    Icecream*** table = create_icecream_table(col, row);
+    for (i = 0; i < row; i++){
+        for (j = 0; j < col; j++){
+            table[j][i] = icecream_tb[i][j];
+        }
+    }
+    free(icecream_tb);
+    icecream_tb = table;
 
+    for(i = 0; i < col; i++){
+        for(j  = 0; j < row; j++){
+            printf("[(%d)%15s]", icecream_tb[i][j]->number, icecream_tb[i][j]->name);
+        }
+        printf("\n");
+    }
+    reverse = (reverse+1)%2;
+}
 
 void owner_init(Owner *ice_owner){
     int i;
     for(i = 0; i < 5; i++){
-        ice_owner->priceOfCupsize[i] = i*100;
+        ice_owner->priceOfCupsize[i] = (i+1)*100;
     }
     ice_owner->total_income = 0;
 }
@@ -69,30 +107,32 @@ Customer* create_customer(){
 }
 
 void cal_sales(Owner *ice_owner, int cupsize){
-    int income = ice_owner->priceOfCupsize[cupsize];
+    int income = ice_owner->priceOfCupsize[cupsize-1];
     ice_owner->total_income = ice_owner->total_income + income;
 }
-//void table_print() {
-    //int i,j;
-    //char print_temp[512];
-    //FILE *fp= fopen("./icecream_ASC.txt","r");
-    //if(fp== NULL){   
-        //printf("file load failed");
-    //}   
-    //while(fgets(print_temp, sizeof(print_temp),fp) ){   
-    //printf("%s",print_temp);
-    //}   
-    //puts("");
-    //fclose(fp);
-    //for (i= 0; i< 2; i++) {   
-        //for (j = 0; j < 2; j++){   
-            //printf("[%15s]",icecream_tb[i][j]);
-        //}   
-        //printf("\n");
-    //}
-//}
+/*void table_print() {
+    int i,j;
+    char print_temp[512];
+    FILE *fp= fopen("./icecream_ASC.txt","r");
+    if(fp== NULL){   
+        printf("file load failed");
+    }   
+    while(fgets(print_temp, sizeof(print_temp),fp) ){   
+    printf("%s",print_temp);
+    }   
+    puts("");
+    fclose(fp);
+    for (i= 0; i< 2; i++) {   
+        for (j = 0; j < 2; j++){   
+            printf("[%15s]",icecream_tb[i][j]);
+        }   
+        printf("\n");
+    }
+}*/
 
 void get_order(Owner* ice_owner){
+    int row;
+    int col;
     Customer* cus = create_customer();
     int i;
     Sleep(1000);
@@ -102,8 +142,14 @@ void get_order(Owner* ice_owner){
     printf("고객 : 저 컵사이즈는 %d번으로 해주시고, 맛은 ",cus->cup_size);
     for(i = 0; i < cus->cup_size; i++){
         int ice_num = rand() % (ROW*COL);
-        int row = ice_num / COL;
-        int col = ice_num % COL;
+        if(reverse == 0){
+            row = ice_num / COL;
+            col = ice_num % COL;
+        }
+        else{
+            row = ice_num % COL;
+            col = ice_num / COL;
+        }
         cus->order[i] = ice_num;
         printf("%d번 %s, ", icecream_tb[row][col]->number, icecream_tb[row][col]->name);
     }
@@ -111,20 +157,20 @@ void get_order(Owner* ice_owner){
     cal_sales(ice_owner, cus->cup_size);
 }
 
-//void orderprint(Customer x){
-//    int i = 0;
-//    printf("%d번손님 %d번 컵사이즈로 다음과 같이 주문하셨습니다. ", x.cstmrnum, x.cup_size);
-//    for (i = 0; i < x.cup_size; i++){
-//        printf(" %d번", x.order[i]);
-//    }
-//    printf(". 잠시만 기다려 주세요.\n");
-//}
+/*void orderprint(Customer x){
+    int i = 0;
+    printf("%d번손님 %d번 컵사이즈로 다음과 같이 주문하셨습니다. ", x.cstmrnum, x.cup_size);
+    for (i = 0; i < x.cup_size; i++){
+        printf(" %d번", x.order[i]);
+    }
+    printf(". 잠시만 기다려 주세요.\n");
+}*/
 
 int main(){
     int k,l;
     table_init();
     for(k = 0; k < 2; k++){
-        for(l = 0; l < 2; l++){
+        for(l = 0; l < 3; l++){
             printf("[%d: %15s]", icecream_tb[k][l]->number, icecream_tb[k][l]->name);
         }
         printf("\n");
@@ -134,6 +180,11 @@ int main(){
     int i = 0;
 
     get_order(&ice_owner);
+    printf("단속원 떴다!\n");
+    transposed_icecream();
+    get_order(&ice_owner);
+    printf("단속원 갔다!\n");
+    transposed_icecream();
     get_order(&ice_owner);
 
     printf("최종 수익: %d\n", ice_owner.total_income);
